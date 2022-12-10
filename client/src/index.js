046bc820687b1b09fc155d119f8993df99f77a8e
Võ Bảo Long <75128770+vobaolong@users.noreply.github.com>
2022-12-10 12:01:48 +0700

    fix layout

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { AuthContextProvider } from './context/AuthContext';
-import { SearchContextProvider } from './context/SearchContext';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { useLocation } from "react-router-dom";
+import App from "./App";
+import { AuthContextProvider } from "./context/AuthContext";
+import { SearchContextProvider } from "./context/SearchContext";
+import ScrollToTop from "react-scroll-to-top";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const ScrollToTops = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <SearchContextProvider>
+        <ScrollToTops />
         <App />
+        <ScrollToTop
+          title="Go to top"
+          smooth
+          style={{
+            justifyContent: "center",
+            display: "grid",
+            alignContent: "center",
+            zIndex: 1000,
+          }}
+        />
       </SearchContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
